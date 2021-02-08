1ff3d768213e534c03a24b12067299a49d2b7e94
Ivan <ivan.milanovic87@gmail.com>
2021-02-08 15:38:05 +0100

    first commit

---
+++ components/CoinList.js
@@ -0,0 +1,5 @@
+export default function CryptoList() {
+    return (
+        <></>
+    )
+}
---
+++ components/Coins/index.js
@@ -0,0 +1,10 @@
+import styles from "../SearchBar/Search.module.css";
+
+
+const Cryptos = () => {
+    return (
+      <></>
+    )
+}
+
+export default Cryptos;
